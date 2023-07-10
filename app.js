e4d110126b2ee5480d54f33decb9ce45462620e4
Vladymyr <vladymyr.svyrydenko2004@gmail.com>
2023-07-10 23:50:43 +0300

    format-code

--- app.js
+++ app.js
@@ -3,32 +3,32 @@ const colors = ['yellow', 'orange', 'blue ', 'purple', 'green', 'red']
 const SQURES_NUMBERS = 400
 
 for (let i = 0; i < SQURES_NUMBERS; i++) {
-    const square = document.createElement('div')
-    square.classList.add('square')
+  const square = document.createElement('div')
+  square.classList.add('square')
 
-    square.addEventListener('mouseover', () => {
-        setColor(square)
-    })
+  square.addEventListener('mouseover', () => {
+    setColor(square)
+  })
 
-    square.addEventListener('mouseleave', () => {
-        removeColor(square)
-    })
+  square.addEventListener('mouseleave', () => {
+    removeColor(square)
+  })
 
-    board.append(square)
+  board.append(square)
 }
 
 function setColor(element) {
-    const color = getRandomColor()
-    element.style.background = color
-    element.style.boxShadow = `0 0 2 px ${color}, 0 0 10px ${color}`
+  const color = getRandomColor()
+  element.style.background = color
+  element.style.boxShadow = `0 0 2 px ${color}, 0 0 10px ${color}`
 }
 
 function removeColor(element) {
-    element.style.background = '#1d1d1d'
-    element.style.boxShadow = `0 0 2 px #000`
+  element.style.background = '#1d1d1d'
+  element.style.boxShadow = `0 0 2 px #000`
 }
 
 function getRandomColor() {
-    const index = (Math.floor(Math.random() * colors.length))
-    return colors[index]
+  const index = Math.floor(Math.random() * colors.length)
+  return colors[index]
 }
